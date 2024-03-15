9d000514106f245dc0f0d9d58a5208747b7c18bf
olive-3 <olive3three@gmail.com>
2024-03-16 04:06:44 +0900

    feat: menu layout, add_discount radio value

--- src/main/java/archivegarden/shop/entity/DiscountType.java
+++ src/main/java/archivegarden/shop/entity/DiscountType.java
@@ -1,7 +1,7 @@
 package archivegarden.shop.entity;
 
 public enum DiscountType {
-    FIX("정액 할인"), RATE("정률 할인");
+    RATE("정률 할인"), FIX("정액 할인");
 
     private final String description;
 
--- src/main/resources/static/js/addDiscount.js
+++ src/main/resources/static/js/addDiscount.js
@@ -1,46 +1,30 @@
-$(document).ready(function() {
-    $('#value').on('focusout', function() {
-        validateDiscountValue();
-    });
+function validateBeforeSubmit() {
+    let discountValue = $('#value').val().trim();
+    let discountType = $('input[name=type]:checked').val();
 
-    $('#discountForm').submit(function(event) {
-        event.preventDefault();
-
-        var isValid = true;
-        isValid = validateDiscountValue() && isValid;
-
-        if (!isValid) {
-            alert('모든 필드를 정확히 입력해주세요.');
-        } else {
-        }
-    });
-
-
-    function validateDiscountValue() {
-        var discountValue = $('#value').val().trim();
-        var discountType = $('input[name=options]:checked').val();
-
-        if (discountValue === '') {
-            alert('할인 설정을 입력해주세요.');
-            return false;
-        } else if (parseInt(discountValue) < 1) {
-            alert('1보다 큰 값을 입력해주세요.');
-            return false;
-        }
-
-        if (discountType === 'option1') {
-            if (!/^[1-9][0-9]?$|^100$/.test(discountValue)) {
-                alert('1부터 100사이의 값을 입력해주세요.');
-                return false;
-            }
-        } else if (discountType === 'option2') {
-            if (parseInt(discountValue) <= 1) {
-                alert('1보다 큰 값을 입력해주세요.');
-                return false;
-            }
-        }
-
-        return true;
+    if (discountValue === '') {
+        alert('할인 설정을 입력해 주세요.');
+        return false;
     }
-});
+
+    if (parseInt(discountValue) < 1) {
+        alert('0보다 큰 값을 입력해 주세요.');
+        return false;
+    }
+
+    if (discountType === 'RATE') {
+        if (!/^[1-9][0-9]?$|^100$/.test(discountValue)) {
+            alert('1부터 100사이의 값을 입력해 주세요.');
+            return false;
+        }
+    } else if (discountType === 'FIX') {
+        if (parseInt(discountValue) < 1) {
+            alert('0보다 큰 값을 입력해 주세요.');
+            return false;
+        }
+    }
+
+    return true;
+}
+
 
