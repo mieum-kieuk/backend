2fadc1cafe2414ff0a4f6acc8deb9e3024eb9eee
jvv00 <jvv0006@gmail.com>
2024-03-27 19:32:59 +0900

    modify: main layout, slide & search function

--- src/main/resources/static/js/comm.js
+++ src/main/resources/static/js/comm.js
@@ -1,10 +1,31 @@
-//스와이퍼
-var swiper = new Swiper(".main_slide", {
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-          },
-        pagination: {
-            el: ".swiper-pagination",
-        },
+$(function () {
+    let now = 0
+    const slideCount = $('.main_slide > ul > li').length
+    const slideList = $('.main_slide > ul > li')
+
+    setInterval(function () {
+        let next = (now + 1) % slideCount
+
+        $(slideList[now]).fadeOut(1500)
+        $(slideList[next]).fadeIn(1500)
+        now = next
+    }, 3000)
+})
+
+$(document).ready(function() {
+    $('.search button').click(function(e) {
+        e.preventDefault();
+
+        $('.search_bg').fadeIn();
+        $('.search_form_wrap').fadeIn();
+
+        e.stopPropagation();
     });
+
+    $(document).click(function(e) {
+        if (!$(e.target).closest('.search_form_wrap').length && !$(e.target).hasClass('search')) {
+            $('.search_bg').fadeOut();
+            $('.search_form_wrap').fadeOut();
+        }
+    });
+});
