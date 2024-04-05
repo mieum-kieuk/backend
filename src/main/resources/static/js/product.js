2ef1746908bdbc21e5a49933a74eb36c62181646
jvv00 <jvv0006@gmail.com>
2024-04-05 17:08:15 +0900

    modify: shop product list & details css

--- src/main/resources/static/js/product.js
+++ src/main/resources/static/js/product.js
@@ -1,34 +1,42 @@
 $(document).ready(function() {
     $('#increaseBtn').click(function(event) {
         event.preventDefault();
-        var input = $(this).siblings('.quant_input');
-        var currentValue = parseInt(input.val());
+        let input = $(this).siblings('.quant_input');
+        let currentValue = parseInt(input.val());
         input.val(currentValue + 1);
     });
 
     $('#decreaseBtn').click(function(event) {
         event.preventDefault();
-        var input = $(this).siblings('.quant_input');
-        var currentValue = parseInt(input.val());
+        let input = $(this).siblings('.quant_input');
+        let currentValue = parseInt(input.val());
         if (currentValue > 1) {
             input.val(currentValue - 1);
         }
     });
 
-
-});
-$(document).ready(function() {
     $('.tab_tit').click(function() {
-        var tabWrap = $(this).siblings('.tab_wrap');
+        let tabWrap = $(this).siblings('.tab_wrap');
         $('.prd_info_tab .tab_wrap:visible').not(tabWrap).slideUp();
         tabWrap.slideToggle();
-
-        // Toggle expand icon
-        $(this).find('.expand_icon').text(function(_, text) {
+        
+        let expandIcon = $(this).find('.expand_icon');
+        $('.tab_tit .expand_icon').not(expandIcon).text('expand_more'); 
+        expandIcon.text(function(_, text) {
             return text === 'expand_more' ? 'expand_less' : 'expand_more';
         });
 
         return false;
     });
+
+    $('.wish').click(function() {
+        $(this).toggleClass('filled');
+        if ($(this).hasClass('filled')) {
+            $(this).text('favorite');
+        } else {
+            $(this).hasClass('wish');
+        }
+    });
 });
+
 
