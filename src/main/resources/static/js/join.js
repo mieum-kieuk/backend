2072b21267105c017988f7b61384ad4b5bd2a22f
jvv00 <jvv0006@gmail.com>
2024-04-15 16:16:51 +0900

    modify: join validation

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,14 +1,4 @@
 $(document).ready(function () {
-    $('input[type="text"]').on('input', function (e) {
-        var maxLength = $(this).attr('maxlength');
-        var textLength = e.target.value.length;
-
-        if (textLength > maxLength) {
-            var trimmedValue = e.target.value.substring(0, maxLength);
-            $(this).val(trimmedValue);
-        }
-    });
-
     $('#loginId').on('focusout', function () {
         isLoginIdValid();
     });
@@ -37,7 +27,9 @@ $(document).ready(function () {
     $('#btn_action_verify_mobile').on('click', function () {
         requestVerificationCode();
     });
-
+    $('#btn_verify_mobile_confirm').click(function () {
+        isVerificationValid();
+    });
     $('input[id^="phonenumber"]').on('input', function () {
         verificationBtnState();
     });
@@ -46,23 +38,26 @@ $(document).ready(function () {
         toggleAgreement(this);
     });
 });
-var interval;
-function isLoginIdValid() {
-    var loginId = $("#loginId").val();
 
-    if (loginId.trim() === '') {
-        $('#idMsg').text('아이디를 입력해주세요.');
-        $('#idMsg').removeClass('success error').addClass('error');
-        return false;
+//아이디 검증
+function isLoginIdValid() {
+    let loginId = $("#loginId").val();
+
+    if (!isLoginIdEmpty()) {
+        return;
     } else if (!regexLoginId(loginId)) {
-        $('#idMsg').text('5~20자의 영문 소문자, 숫자 조합을 사용해 주세요.');
-        $('#idMsg').removeClass('success').addClass('error');
-        return false;
+        return;
     }
+
+    $('#idMsg').text('');
+
     $.ajax({
         type: 'POST',
         url: '/members/verification/loginId',
-        data: { loginId: loginId },
+        data: {loginId: loginId},
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader(csrfHeader, csrfToken);
+        },
         success: function (result) {
             if (result) {
                 $('#idMsg').text('사용 가능한 아이디입니다.');
@@ -73,92 +68,133 @@ function isLoginIdValid() {
             }
         }
     });
-    $('#idMsg').text('');
+
+    return;
+}
+
+function isLoginIdEmpty() {
+    let loginId = $("#loginId").val();
+
+    if (loginId.trim() === '') {
+        $('#idMsg').text('아이디를 입력해 주세요.');
+        $('#idMsg').removeClass('success error').addClass('error');
+        return false;
+    }
+
     return true;
 }
 
-function regexLoginId(loginId) {
-    var regex = /^(?=.*[a-z])(?=.*\d)[a-z\d]{5,20}$/;
-    return regex.test(loginId);
+function regexLoginId() {
+    let loginId = $("#loginId").val();
+    let regex = /^(?=.*[a-z])(?=.*\d)[a-z\d]{5,20}$/;
+    if (!regex.test(loginId)) {
+        $('#idMsg').text('5~20자의 영문 소문자, 숫자 조합을 사용해 주세요.');
+        $('#idMsg').removeClass('success').addClass('error');
+        return false;
+    }
+
+    return true;
 }
 
+//비밀번호 검증
 function isPasswordValid() {
-    var password = $("#password").val();
-    var confirmPassword = $('#passwordConfirm').val();
-
-    if (password.trim() === '') {
-        $('#pwMsg').text('비밀번호를 입력해주세요.');
-        return false;
-    } else if (!regexPassword(password)) {
-        $('#pwMsg').text('8~16자의 영문 대/소문자, 숫자, 특수문자 조합을 사용해 주세요.');
-        return false;
+    if (!isPasswordEmpty()) {
+        return;
+    } else if (!regexPassword()) {
+        return;
     }
 
     $('#pwMsg').text('');
 
+    return;
+}
+
+function isPasswordEmpty() {
+    let password = $("#password").val();
+
+    if (password.trim() === '') {
+        $('#pwMsg').text('비밀번호를 입력해 주세요.');
+        $('#pwMsg').removeClass('success error').addClass('error');
+        return false;
+    }
+
+    return true;
+}
+
+function regexPassword() {
+    let password = $("#password").val();
+    let regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&^()])[a-zA-Z\d@$!%*?&^()]{8,16}$/;
+    if (!regex.test(password)) {
+        $('#pwMsg').text('8~16자의 영문 대/소문자, 숫자, 특수문자 조합을 사용해 주세요.');
+        return false;
+    }
+
+    return true;
+}
+
+//비밀번호 확인 검증
+function isPwConfirmValid() {
+    let password = $('#password').val();
+    let confirmPassword = $('#passwordConfirm').val();
+
     if (password === confirmPassword) {
         $('#pwconfirmMsg').text('');
-    }
-
-    return true;
-}
-
-function regexPassword(password) {
-    var regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^()])[A-Za-z\d@$!%*?&^()]{8,16}$/;
-    return regex.test(password);
-}
-
-function isPwConfirmValid() {
-    var password = $('#password').val();
-    var confirmPassword = $('#passwordConfirm').val();
-
-    if (confirmPassword === '') {
-        $('#pwconfirmMsg').text('');
+        return true;
     } else {
-        if (password === confirmPassword) {
-            $('#pwconfirmMsg').text('');
-        } else {
-            $('#pwconfirmMsg').text('비밀번호가 일치하지 않습니다.');
-            return false;
-        }
+        $('#pwconfirmMsg').text('비밀번호가 일치하지 않습니다.');
+        return false;
     }
-    return true;
 }
 
+//이름 검증
 function isNameValid() {
-    var name = $('#name').val();
+    if (!isNameEmpty()) {
+        return;
+    } else if (!regexName()) {
+        return;
+    }
+    $('#nameMsg').text('');
+    return;
+}
+function isNameEmpty() {
+    let name = $("#name").val();
 
     if (name.trim() === '') {
-        $('#nameMsg').text('이름을 입력해주세요.');
+        $('#nameMsg').text('이름을 입력해 주세요.');
+        $('#nameMsg').removeClass('success error').addClass('error');
         return false;
-    } else if (!regexName(name)) {
+    }
+
+    return true;
+}
+
+function regexName() {
+    let name = $('#name').val();
+    let regex = /^[가-힣a-zA-Z]{2,12}$/;
+    if (!regex.test(name)) {
         $('#nameMsg').text('한글, 영문 대/소문자를 사용해 주세요. (특수기호, 공백 사용 불가)');
         return false;
     }
 
-    $('#nameMsg').text('');
     return true;
 }
 
-function regexName(name) {
-    var regex = /^[가-힣a-zA-Z]{2,12}$/;
-    return regex.test(name);
-}
-
+//이메일 검증
 function isEmailValid() {
-    var email = $('#email').val();
+    let email = $('#email').val();
 
-    if (email.trim() === '') {
-        $('#emailMsg').text('이메일을 입력해주세요.');
-        return false;
-    } else if (!regexEmail(email)) {
-        $('#emailMsg').text('유효한 이메일을 입력해주세요.');
-        return false;
+    if (!isEmailEmpty()) {
+        return;
+    } else if (!regexEmail()) {
+        return;
     }
+
+    $('#emailMsg').text('');
+
     $.ajax({
         type: 'POST',
         url: '/members/verification/email',
-        data: { email: email },
+        data: {email: email},
         success: function (result) {
             if (result) {
                 $('#emailMsg').text('사용 가능한 이메일입니다.');
@@ -167,43 +203,85 @@ function isEmailValid() {
                 $('#emailMsg').text('이미 사용 중인 이메일입니다.');
                 $('#emailMsg').removeClass('success').addClass('error');
             }
+        },
+        error: function () {
+            // 오류 발생 시 처리
+            $('#emailMsg').text('유효한 이메일을 입력해 주세요.');
+            $('#emailMsg').removeClass('success').addClass('error');
         }
     });
-    return true;
+
+    return;
 }
 
-function regexEmail(email) {
-    var regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    return regex.test(email);
-}
+function isEmailEmpty() {
+    let email = $('#email').val();
 
-function isPhoneValid() {
-    var phonenumber1 = $('#phonenumber1').val();
-    var phonenumber2 = $('#phonenumber2').val();
-    var phonenumber3 = $('#phonenumber3').val();
-
-    if (phonenumber1.trim() === '' || phonenumber2.trim() === '' || phonenumber3.trim() === '') {
-        $('#phoneNumberMsg').text('휴대전화번호를 입력해 주세요.');
-        return false;
-    } else if (!regexPhone(phonenumber1, phonenumber2, phonenumber3)) {
-        $('#phoneNumberMsg').text('유효한 휴대전화번호를 입력해 주세요.');
+    if (email.trim() === '') {
+        $('#emailMsg').text('이메일을 입력해 주세요.');
         return false;
     }
 
-    $('#phoneNumberMsg').text('');
     return true;
 }
 
-function regexPhone(phonenumber1, phonenumber2, phonenumber3) {
-    var regex1 = /^[0-9]{3,4}$/;
-    var regex2 = /^[0-9]{4}$/;
-    return regex1.test(phonenumber1) && regex1.test(phonenumber2) && regex2.test(phonenumber3);
+function regexEmail() {
+    let email = $('#email').val();
+    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!regex.test(email)) {
+        $('#emailMsg').text('유효한 이메일을 입력해 주세요.');
+        $('#emailMsg').removeClass('success').addClass('error');
+        return false;
+    }
+
+    return true;
 }
+
+//휴대전화번호 검증
+function isPhoneValid() {
+    if (!isPhoneEmpty()) {
+        return;
+    } else if (!regexPhone()) {
+        return;
+    }
+
+    $('#phoneNumberMsg').text('');
+    return;
+}
+
+function isPhoneEmpty() {
+    let phonenumber2 = $('#phonenumber2').val();
+    let phonenumber3 = $('#phonenumber3').val();
+
+    if (phonenumber2.trim() === '' && phonenumber3.trim() === '') {
+        $('#phoneNumberMsg').text('휴대전화번호를 입력해 주세요.');
+        return false;
+    }
+
+    return true;
+}
+
+function regexPhone() {
+    let phonenumber2 = $('#phonenumber2').val();
+    let phonenumber3 = $('#phonenumber3').val();
+    let regex1 = /^[0-9]{3,4}$/;
+    let regex2 = /^[0-9]{4}$/;
+    if ( regex1.test(phonenumber2) && regex2.test(phonenumber3)) {
+        return true;
+    }else {
+        $('#phoneNumberMsg').text('유효한 휴대전화번호를 입력해 주세요.');
+        return false;
+    }
+}
+
+//타이머 설정
+let interval;
+
 function startTimer() {
-    var timer = 180; // 3분 = 180초
+    let timer = 180; // 3분 = 180초
     interval = setInterval(function () {
-        var minutes = Math.floor(timer / 60);
-        var seconds = timer % 60;
+        let minutes = Math.floor(timer / 60);
+        let seconds = timer % 60;
 
         $('#expiryTime').text(minutes + ':' + (seconds < 10 ? '0' + seconds : seconds));
 
@@ -216,15 +294,16 @@ function startTimer() {
         }
     }, 1000);
 }
+
 function requestVerificationCode() {
-    var phonenumber1 = $('#phonenumber1').val();
-    var phonenumber2 = $('#phonenumber2').val();
-    var phonenumber3 = $('#phonenumber3').val();
+    let phonenumber1 = $('#phonenumber1').val();
+    let phonenumber2 = $('#phonenumber2').val();
+    let phonenumber3 = $('#phonenumber3').val();
 
     $.ajax({
         type: 'POST',
         url: '/members/send/verificationNo',
-        data: { phonenumber1: phonenumber1, phonenumber2: phonenumber2, phonenumber3: phonenumber3 },
+        data: {phonenumber1: phonenumber1, phonenumber2: phonenumber2, phonenumber3: phonenumber3},
         dataType: 'json',
         success: function (result) {
             if (result['success']) {
@@ -242,13 +321,14 @@ function requestVerificationCode() {
     });
 }
 
+//인증번호 검증
 function verificationBtnState() {
-    var phonenumber1 = $('#phonenumber1').val();
-    var phonenumber2 = $('#phonenumber2').val();
-    var phonenumber3 = $('#phonenumber3').val();
-    var regex1 = /^[0-9]{3,4}$/;
-    var regex2 = /^[0-9]{4}$/;
-    var isValidPhoneNumber = regex1.test(phonenumber1) && regex1.test(phonenumber2) && regex2.test(phonenumber3);
+    let phonenumber1 = $('#phonenumber1').val();
+    let phonenumber2 = $('#phonenumber2').val();
+    let phonenumber3 = $('#phonenumber3').val();
+    let regex1 = /^[0-9]{3,4}$/;
+    let regex2 = /^[0-9]{4}$/;
+    let isValidPhoneNumber = regex1.test(phonenumber1) && regex1.test(phonenumber2) && regex2.test(phonenumber3);
 
     if (isValidPhoneNumber) {
         $('#btn_action_verify_mobile').removeClass('disabled');
@@ -262,18 +342,52 @@ function verificationBtnState() {
     clearInterval(interval);
     $('#expiryTime').text('');
 }
-function isVerificationValid() {
-    var verificationCode = $('#verificationNo').val();
 
-    if (verificationCode.trim() === '') {
+function isVerificationValid() {
+    let phonenumber1 = $('#phonenumber1').val();
+    let phonenumber2 = $('#phonenumber2').val();
+    let phonenumber3 = $('#phonenumber3').val();
+    let verificationNo = $('#verificationNo').val();
+
+    $.ajax({
+        type: 'POST',
+        url: '/members/verification/verificationNo',
+        data: {
+            phonenumber1: phonenumber1,
+            phonenumber2: phonenumber2,
+            phonenumber3: phonenumber3,
+            verificationNo: verificationNo
+        },
+        success: function (result) {
+            if (result) {
+                $('#expiryTime').css('display', 'none');
+                $('#verificationNo').val('');
+                $('#verificationNo').attr('placeholder', '휴대전화 인증 완료');
+                $('#verificationNo').attr('disabled', true);
+                $('#verificationNo').css('background-color', '#EFEFEF');
+                $('#btn_verify_mobile_confirm').attr('disabled', true);
+                $('#btn_verify_mobile_confirm').css('border', '1px solid #999');
+                isValidVerificationCode = true;
+            } else {
+                $('#verificationNo').val('');
+                alert('인증번호가 일치하지 않습니다.\n확인 후 다시 시도해 주세요.');
+                isValidVerificationCode = false;
+            }
+        }
+    });
+}
+
+function isVerificationEmpty() {
+    let placeholderValue = $('#verificationNo').attr('placeholder');
+    if (placeholderValue !== '휴대전화 인증 완료') {
         return false;
     }
-
     return true;
 }
 
+//전체 선택
 function toggleAgreement(element) {
-    var isChecked = $(element).prop('checked');
+    let isChecked = $(element).prop('checked');
 
     if (isChecked) {
         $(element).parents("#agree_group").find('input[type="checkbox"]').prop("checked", true);
@@ -282,41 +396,71 @@ function toggleAgreement(element) {
     }
 }
 
-function signUp() {
-    if (!isLoginIdValid()) {
+function validateBeforeSubmit() {
+    // 아이디 유효성 검사
+    if (!isLoginIdEmpty()) {
         alert("아이디를 입력해 주세요.");
         return false;
-    }
-    if (!isPasswordValid()) {
-        alert("비밀번호를 입력해 주세요.");
+    } else if (!regexLoginId()) {
+        alert("유효한 아이디를 입력해 주세요.");
         return false;
     }
+
+    // 비밀번호 유효성 검사
+    if (!isPasswordEmpty()) {
+        alert("비밀번호를 입력해 주세요.");
+        return false;
+    } else if (!regexPassword()) {
+        alert("유효한 비밀번호를 입력해 주세요.");
+        return false;
+    }
+
+    // 비밀번호 확인 유효성 검사
     if (!isPwConfirmValid()) {
         alert("비밀번호가 일치하지 않습니다.");
         return false;
     }
-    if (!isNameValid()) {
+
+    // 이름 유효성 검사
+    if (!isNameEmpty()) {
         alert("이름을 입력해 주세요.");
         return false;
-    }
-    if (!isPhoneValid()) {
-        alert("휴대전화번호를 입력해 주세요.");
-        return false;
-    }
-    if (!isVerificationValid()) {
-        alert("휴대전화번호 인증해 주세요.");
-        return false;
-    }
-    if (!isEmailValid()) {
-        alert("이메일을 입력해 주세요.");
+    } else if (!regexName()) {
+        alert("유효한 이름을 입력해 주세요.");
         return false;
     }
 
+    // 휴대전화번호 유효성 검사
+    if (!isPhoneEmpty()) {
+        alert("휴대전화번호를 입력해 주세요.");
+        return false;
+    } else if (!regexPhone()) {
+        alert("유효한 휴대전화번호를 입력해 주세요.");
+        return false;
+    }
+
+    // 인증 번호 유효성 검사
+    if (!isVerificationEmpty()) {
+        alert("휴대전화번호 인증해 주세요.");
+        return false;
+    }
+
+    // 이메일 유효성 검사
+    if (!isEmailEmpty()) {
+        alert("이메일을 입력해 주세요.");
+        return false;
+    } else if (!regexEmail()) {
+        alert("유효한 이메일을 입력해 주세요.");
+        return false;
+    }
+
+    // 이용약관 동의 확인
     if (!$('#agree_to_terms_of_use').is(':checked')) {
         alert("이용약관 동의를 확인해 주세요.");
         return false;
     }
 
+    // 개인정보 수집 및 이용 동의 확인
     if (!$('#agree_to_personal_information').is(':checked')) {
         alert("개인정보 수집 및 이용 동의를 확인해 주세요.");
         return false;
@@ -324,3 +468,4 @@ function signUp() {
 
     return true;
 }
+
