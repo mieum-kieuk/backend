d539818fac31f0deca0aaeb9cf220a593b3c58ab
jvv00 <jvv0006@gmail.com>
2024-05-05 20:28:02 +0900

    modify: cart layout & updateTotalPrice js

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -5,11 +5,32 @@ $(document).ready(function() {
         $('.cart_checkbox').prop('checked', isChecked);
         updateTotalPrice();
     });
-
-    // 상품 체크박스 변경 시
+// 체크박스 변경 시
     $('.cart_checkbox').change(function() {
         updateTotalPrice();
+        updatePurchaseButton();
     });
+
+// 구매하기 버튼 상태 업데이트 함수
+    function updatePurchaseButton() {
+        var isChecked = false;
+
+        $('.cart_checkbox').each(function() {
+            if ($(this).prop('checked')) {
+                isChecked = true;
+                return false; // 반복문 종료
+            }
+        });
+
+        if (isChecked) {
+            $('.submit_btn').prop('disabled', false);
+            $('.submit_btn').removeClass('disabled'); // 버튼 활성화 시 disabled 클래스 제거
+        } else {
+            $('.submit_btn').prop('disabled', true);
+            $('.submit_btn').addClass('disabled');
+        }
+    }
+
 
     $('.increase').click(function(event) {
         event.preventDefault();
@@ -41,33 +62,61 @@ $(document).ready(function() {
         $(this).closest('.cart_item').remove();
         updateTotalPrice();
     });
+    $("#deleteAllBtn").click(function() {
+        if (confirm("정말 삭제하시겠습니까?")) {
 
+            $(".cart_item").remove();
+            alert("전체 삭제되었습니다.");
+        }
+    });
+
+    $("#deleteBtn").click(function() {
+        var checkedItems = $("input[name='cartCheckBox']:checked");
+        if (checkedItems.length === 0) {
+            alert("상품을 선택해 주세요.");
+        } else {
+            if (confirm("정말 삭제하시겠습니까?")) {
+
+                checkedItems.closest('.cart_item').remove();
+                alert("선택된 상품이 삭제되었습니다.");
+            }
+        }
+    });
     // 가격 업데이트 함수
     function updateTotalPrice() {
         var totalPrice = 0;
         var totalItemsPrice = 0;
+        var totalDiscountPrice = 0;
 
-        // 선택된 상품의 가격 합계 계산
+        // 선택된 상품의 가격 합계 및 할인 금액 계산
         $('.cart_item').each(function() {
             if ($(this).find('.cart_checkbox').prop('checked')) {
                 var quantity = parseInt($(this).find('.quant_input').val());
-                var price = parseInt($(this).find('.item.price').text().replace('원', '').replace(',', ''));
-                var itemTotalPrice = quantity * price;
+                var originalPrice = parseFloat($(this).find('.item.info .original_price #productPrice').text().replace('원', '').replace(',', ''));
+                var discountPrice = parseFloat($(this).find('.item.info .sale_price .discount_price').text().replace('원', '').replace(',', ''));
+                var salePrice = originalPrice - discountPrice;
+                var itemTotalPrice = quantity * discountPrice;
+                var itemDiscountPrice = quantity * salePrice;
+
                 totalPrice += itemTotalPrice;
-                totalItemsPrice += itemTotalPrice;
-                $(this).find('.item_total_price').text(addCommas(itemTotalPrice) + '원');
+                totalItemsPrice += quantity * originalPrice; // 할인 적용 전 가격 합계
+                totalDiscountPrice += itemDiscountPrice;
+
+                $(this).find('.item.price .sale_price').text(addCommas(itemTotalPrice) + '원');
             }
         });
 
         // 상품 합계 업데이트
         $('.total.price .content').text(addCommas(totalItemsPrice) + '원');
 
-        // 배송비 합산
-        var shippingFee = parseInt($('.total.shipping .content').text().replace('원', '').replace(',', ''));
-        totalPrice += shippingFee;
+        // 상품 할인 금액 업데이트
+        $('.total.discount .content').text('-' + addCommas(totalDiscountPrice) + '원');
 
-        // 합계 업데이트
-        $('.total_price .content').text(addCommas(totalPrice) + '원');
+        // 결제 예정 금액 계산 및 업데이트
+        var paymentAmount = totalItemsPrice - totalDiscountPrice;
+        var shippingFee = parseInt($('.total.shipping .content').text().replace('원', '').replace(',', ''));
+        paymentAmount += shippingFee;
+        $('.total_price .content').text(addCommas(paymentAmount) + '원');
     }
 
     // 콤마 추가 함수
