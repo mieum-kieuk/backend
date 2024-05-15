4ba8b7c8616eebba38429b2a54e97c7d4f028058
jvv00 <jvv0006@gmail.com>
2024-05-15 17:12:44 +0900

    modify: cart updateTotalPrice js & soldout quantity btn

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -11,6 +11,14 @@ $(document).ready(function () {
         updatePurchaseButton();
     });
 
+    $('.quant_input').each(function() {
+        let currentValue = parseInt($(this).val());
+        let decreaseBtn = $(this).siblings('.decrease');
+        if (currentValue === 1) {
+            decreaseBtn.addClass('disabled');
+        }
+    });
+
 // 구매하기 버튼 상태 업데이트 함수
     function updatePurchaseButton() {
         var isChecked = false;
@@ -32,63 +40,94 @@ $(document).ready(function () {
     }
 
 
-    $('.increase').click(function (event) {
+    $('.increase').click(function(event) {
         event.preventDefault();
         let input = $(this).siblings('.quant_input');
         let currentValue = parseInt(input.val());
-        input.val(currentValue + 1);
-        updateTotalPrice();
-    });
+        let cartItem = $(this).closest('.cart_item');
+        let decreaseBtn = $(this).siblings('.decrease');
 
-    $('.decrease').click(function (event) {
-        event.preventDefault();
-        let input = $(this).siblings('.quant_input');
-        let currentValue = parseInt(input.val());
-        if (currentValue > 1) {
-            input.val(currentValue - 1);
+        if (!cartItem.hasClass('soldout')) {
+            input.val(currentValue + 1);
+            if (currentValue === 1) {
+                decreaseBtn.removeClass('disabled');
+            }
             updateTotalPrice();
-        } else {
-            alert("최소 주문 수량은 1개 입니다.");
+        }
+        else {
+            $(this).addClass('disabled');
         }
     });
 
+    $('.decrease').click(function(event) {
+        event.preventDefault();
+        let input = $(this).siblings('.quant_input');
+        let currentValue = parseInt(input.val());
+        let cartItem = $(this).closest('.cart_item');
+        let increaseBtn = $(this).siblings('.increase');
+
+        if (!cartItem.hasClass('soldout') && currentValue > 1) {
+            input.val(currentValue - 1);
+            if (currentValue === 2) {
+                $(this).addClass('disabled');
+            }
+            updateTotalPrice();
+        }else {
+            $(this).addClass('disabled');
+        }
+    });
+
+
+
     // 수량 변경 시
-    $('.quant_input').change(function () {
+    $('.quant_input').change(function() {
         updateTotalPrice();
     });
 
     // 가격 업데이트 함수
     function updateTotalPrice() {
         var totalPrice = 0;
-        var totalItemsPrice = 0;
-        var totalDiscountPrice = 0;
+        var cartProductPrice = 0;
+        var cartDiscountPrice = 0;
 
         // 선택된 상품의 가격 합계 및 할인 금액 계산
-        $('.cart_item').each(function () {
-            if ($(this).find('.cart_checkbox').prop('checked')) {
+        $('.cart_item').each(function() {
+            var isChecked = $(this).find('.cart_checkbox').prop('checked');
+            if (isChecked) {
                 var quantity = parseInt($(this).find('.quant_input').val());
                 var originalPrice = parseFloat($(this).find('.item.info .original_price #productPrice').text().replace('원', '').replace(',', ''));
-                var discountPrice = parseFloat($(this).find('.item.info .sale_price .discount_price').text().replace('원', '').replace(',', ''));
-                var salePrice = originalPrice - discountPrice;
-                var itemTotalPrice = quantity * discountPrice;
-                var itemDiscountPrice = quantity * salePrice;
+
+                var discountPriceElement = $(this).find('.item.info .sale_price #productDiscountPrice');
+                var discountPrice = 0;
+                if (discountPriceElement.length > 0) {
+                    // 할인가가 있는 경우
+                    discountPrice = parseFloat(discountPriceElement.text().replace('원', '').replace(',', ''));
+                    $(this).find('.item.price #productSalePrice').text(addCommas(discountPrice * quantity) + '원');
+                } else {
+                    // 할인가가 없는 경우
+                    discountPrice = originalPrice;
+                    var salePrice = originalPrice;
+                    $(this).find('.item.price #productSalePrice').text(addCommas(salePrice * quantity) + '원');
+                }
+
+                var itemTotalPrice = quantity * discountPrice; // 할인된 가격으로 계산
+                var itemOriginalPrice = quantity * originalPrice;
+                var itemDiscountPrice = itemOriginalPrice - itemTotalPrice;
 
                 totalPrice += itemTotalPrice;
-                totalItemsPrice += quantity * originalPrice; // 할인 적용 전 가격 합계
-                totalDiscountPrice += itemDiscountPrice;
-
-                $(this).find('.item.price .sale_price').text(addCommas(itemTotalPrice) + '원');
+                cartProductPrice += itemOriginalPrice; // 할인 적용 전 가격 합계
+                cartDiscountPrice += itemDiscountPrice;
             }
         });
 
         // 상품 합계 업데이트
-        $('.total.price .content').text(addCommas(totalItemsPrice) + '원');
+        $('.total.price .content').text(addCommas(cartProductPrice) + '원');
 
         // 상품 할인 금액 업데이트
-        $('.total.discount .content').text('-' + addCommas(totalDiscountPrice) + '원');
+        $('.total.discount .content').text('-' + addCommas(cartDiscountPrice) + '원');
 
         // 결제 예정 금액 계산 및 업데이트
-        var paymentAmount = totalItemsPrice - totalDiscountPrice;
+        var paymentAmount = cartProductPrice - cartDiscountPrice;
         var shippingFee = parseInt($('.total.shipping .content').text().replace('원', '').replace(',', ''));
         paymentAmount += shippingFee;
         $('.total_price .content').text(addCommas(paymentAmount) + '원');
