fc1957e2194a65cf5de470819a4f5e4cfc42b5fe
jvv00 <jvv0006@gmail.com>
2024-05-29 13:14:11 +0900

    modify: wish list button layout

--- src/main/resources/static/js/wishList.js
+++ src/main/resources/static/js/wishList.js
@@ -14,17 +14,13 @@ $(document).ready(function() {
     });
 
     $("#deleteBtn").click(function() {
-        var checkedItems = $("input[name='wishCheckBox']:checked");
-        if (checkedItems.length === 0) {
-            alert("상품을 선택해 주세요.");
-        } else {
-            if (confirm("정말 삭제하시겠습니까?")) {
-
-                checkedItems.closest('.wish_item').remove();
-                alert("선택된 상품이 삭제되었습니다.");
-            }
+        var wishItem = $(this).closest('.wish_item');
+        if (confirm("정말 삭제하시겠습니까?")) {
+            wishItem.remove();
+            alert("상품이 삭제되었습니다.");
         }
     });
+
     $('#orderBtn').click(function() {
         $('#addCart').fadeIn();
     });
