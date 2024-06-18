4be5906f5b1f8b0a2c1f46b3b705bb4a7511ce6f
jvv00 <jvv0006@gmail.com>
2024-06-18 10:46:27 +0900

    feat: add point modal

--- src/main/resources/static/js/memberList.js
+++ src/main/resources/static/js/memberList.js
@@ -1,5 +1,51 @@
+$(document).ready(function() {
+
+    $('#startDate, #endDate').datepicker({
+        dateFormat: 'yy-mm-dd',
+        maxDate: 0
+
+    });
+    $('.date_option input[type="button"]').click(function() {
+        $('.date_option input[type="button"]').removeClass('active');
+        $(this).addClass('active');
+    });
+    // 초기화 버튼 클릭 시 검색폼 초기화
+    $('.btn_wrap .reset_btn').click(function () {
+        $('#searchKeyword').val(''); // 검색어 입력 초기화
+        $('#startDate, #endDate').val(''); // 시작일, 종료일 초기화
+    });
+});
+function setSearchDate(days) {
+    if (days === 'all') {
+        $('#startDate').datepicker('setDate', null);
+        $('#endDate').datepicker('setDate', null);
+    } else {
+        let endDate = new Date();
+        let startDate = new Date();
+
+        if (days === 0) {
+            startDate = endDate;
+        } else {
+            startDate.setDate(startDate.getDate() - days);
+        }
+
+        $('#startDate').datepicker('setDate', formatDate(startDate));
+        $('#endDate').datepicker('setDate', formatDate(endDate));
+    }
+}
+
+function formatDate(date) {
+    let d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
 function validateBeforeSubmit() {
-    let searchKeyword = $('#searchKeyword').val().trim();
     let startDatetime = $('#startDate').val().trim();
     let endDatetime = $('#endDate').val().trim();
 
@@ -15,10 +61,6 @@ function validateBeforeSubmit() {
         return true;
     }
 
-    if (searchKeyword === '') {
-        alert('검색어를 입력해 주세요.');
-        return false;
-    }
 
     let startDate = new Date(startDatetime);
     let endDate = new Date(endDatetime);
--- src/main/resources/static/js/point.js
+++ src/main/resources/static/js/point.js
@@ -1,3 +1,42 @@
+$(document).ready(function() {
+    $('.point_btn').click(function() {
+        let selectedMember = $('.admins_table tbody tr input[type=checkbox]:checked');
+        if (selectedMember.length === 0) {
+            alert('회원을 선택해 주세요.');
+            return;
+        }
+
+        let userId = selectedMember.closest('tr').find('td:nth-child(4)').text().trim();
+        $('#userId').val(userId);
+
+        $('#pointModal').css('display', 'flex');
+    });
+
+    let closeBtn = $(".modal_head .close");
+    let cancelBtn = $(".btn_wrap .cancel_btn");
+
+    closeBtn.add(cancelBtn).click(function() {
+        $('#pointModal').hide();
+        $('#addPointForm')[0].reset();
+    });
+
+    $(window).click(function(event) {
+        if (event.target.id === "pointModal") {
+            $('#pointModal').hide();
+            $('#addPointForm')[0].reset();
+        }
+    });
+
+    $('.reset_btn').click(function() {
+        $('#adminsSearchForm')[0].reset();
+    });
+});
+
+// 회원 선택 시 스타일 변경 및 선택 상태 관리
+$('.admins_table tbody tr').click(function() {
+    $(this).toggleClass('selected').siblings().removeClass('selected');
+});
+
 function validateBeforeSubmit() {
     let userId = $('#userId').val().trim();
     let pointContent = $('#pointContent').val().trim();
