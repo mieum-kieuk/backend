b9f012f5c79b1d66a10053569afb1437ab4389ff
jvv00 <jvv0006@gmail.com>
2024-06-19 02:36:49 +0900

    modify: add search form to admin discount

--- src/main/resources/static/js/memberList.js
+++ src/main/resources/static/js/memberList.js
@@ -12,10 +12,13 @@ $(document).ready(function() {
     // 초기화 버튼 클릭 시 검색폼 초기화
     $('.btn_wrap .reset_btn').click(function () {
         $('#searchKeyword').val(''); // 검색어 입력 초기화
-        $('select').val(''); // 검색어 입력 초기화
+        $('select').each(function() {
+            $(this).val($(this).find('option:first').val()); // select 요소 초기화
+        });
         $('#startDate, #endDate').val(''); // 시작일, 종료일 초기화
-        $('.date_option input[type="button"]').removeClass('active');
+        $('.date_option input[type="button"]').removeClass('active'); // active 클래스 제거
     });
+
 });
 function setSearchDate(days) {
     if (days === 'all') {
