354f9ed8535804eb950575d834c13b05bb7a8f3b
jvv00 <jvv0006@gmail.com>
2024-07-21 09:19:35 +0900

    feat: 할인등록 적용상품 항목 추가

--- src/main/resources/static/js/adminDiscount.js
+++ src/main/resources/static/js/adminDiscount.js
@@ -6,41 +6,60 @@ $(document).ready(function () {
     $('#startedAt').attr('min', minDate);
     $('#expiredAt').attr('min', minDate);
 
-    //목록에서 체크박스 전체선택
+    // 목록에서 체크박스 전체선택
     $('#selectAll').click(function () {
-        if ($(this).prop('checked')) {
-            $('.list_content input[type="checkbox"]').prop('checked', true);
+        let isChecked = $(this).prop('checked');
+        $('.result_table input[type="checkbox"]').prop('checked', isChecked);
+        $('.list_content input[type="checkbox"]').prop('checked', isChecked);
+    });
+
+    $('.result_table').on('change', 'input[type="checkbox"]', function () {
+        let totalCheck = $('.result_table tbody input[type="checkbox"]').length ;
+        let checkedBox = $('.result_table tbody input[type="checkbox"]:checked').length ;
+
+        $('#selectAll').prop('checked', totalCheck === checkedBox);
+    });
+
+    $('.list_content').on('change', 'input[type="checkbox"]', function () {
+        let totalCheck = $('.list_content .list input[type="checkbox"]').length;
+        let checkedBox = $('.list_content .list input[type="checkbox"]:checked').length;
+
+        $('#selectAll').prop('checked', totalCheck === checkedBox);
+    });
+
+    $('input[name="productType"]').on('change', function() {
+        if ($(this).val() === 'specific') {
+            $('.input_box_wrap.product').show();
         } else {
-            $('.list_content input[type="checkbox"]').prop('checked', false);
+            $('.input_box_wrap.product').hide();
+        }
+    });
+    $('#popupBtn').click(function () {
+        window.open("./discount_popup.html", "_blank", "width=600px,height=450px");
+    });
+    $('.submit_btn').click(function(event) {
+        if (!validateBeforeSubmit()) {
+            event.preventDefault();
         }
     });
 
 });
-
 //유효성 검사
 function validateBeforeSubmit() {
     let discountName = $('#name').val().trim();
     let discountPercent = $('#discountPercent').val().trim();
+    let productType = $('input[name="productType"]:checked').val();
     let startedAt = $('#startedAt').val().trim();
     let expiredAt = $('#expiredAt').val().trim();
-
+    console.log('Discount Name:', discountName);
+    console.log('Discount Percent:', discountPercent);
+    console.log('Started At:', startedAt);
+    console.log('Expired At:', expiredAt);
+    console.log('Product Type:', productType);
     if (discountName === '') {
         alert('할인명을 입력해 주세요.');
         return false;
     }
-
-    if (discountPercent === '') {
-        alert('할인율을 입력해 주세요.');
-        return false;
-    }
-
-
-    let discountPercentRegex = /^(?:1|[1-9]\d?|100)$/;
-    if (!discountPercentRegex.test(discountPercent)) {
-        alert('1부터 100 사이의 값을 입력해 주세요.');
-        return false;
-    }
-
     if (startedAt === '' && expiredAt === '') {
         alert('할인 기간을 지정해 주세요.');
         return false;
@@ -60,6 +79,28 @@ function validateBeforeSubmit() {
     if (startDate >= endDate) {
         alert('시작일시는 종료일시보다 이전이어야 합니다.');
         return false;
+    }
+    if (discountPercent === '') {
+        alert('할인율을 입력해 주세요.');
+        return false;
+    }
+
+    let discountPercentRegex = /^(?:1|[1-9]\d?|100)$/;
+    if (!discountPercentRegex.test(discountPercent)) {
+        alert('1부터 100 사이의 값을 입력해 주세요.');
+        return false;
+    }
+
+    if (!productType) {
+        alert('적용 상품을 선택해 주세요.');
+        return false;
+    }
+    if (productType === 'specific') {
+        let listItems = $('.list_item');
+        if (listItems.length === 0) {
+            alert('상품을 선택해 주세요.');
+            return false;
+        }
     }
 
     return true;
