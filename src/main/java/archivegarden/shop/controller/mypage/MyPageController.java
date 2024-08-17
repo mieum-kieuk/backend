81c4f1e7749aee281b06d191033764bada4c2efa
olive-3 <olive3three@gmail.com>
2024-08-17 17:11:24 +0900

    fix: swal css, script 헤더에 추가

--- src/main/java/archivegarden/shop/controller/mypage/MyPageController.java
+++ src/main/java/archivegarden/shop/controller/mypage/MyPageController.java
@@ -1,17 +1,15 @@
-package archivegarden.shop.controller;
+package archivegarden.shop.controller.mypage;
 
-import archivegarden.shop.dto.mypage.MyQnaListDto;
 import archivegarden.shop.entity.Member;
-import archivegarden.shop.service.community.QnaService;
+import archivegarden.shop.service.member.MemberService;
+import archivegarden.shop.service.point.SavedPointService;
 import archivegarden.shop.web.annotation.CurrentUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class MyPageController {
 
-    private final QnaService qnaService;
+//    private final QnaService qnaService;
+    private final MemberService memberService;
+    private final SavedPointService savedPointService;
 
     @GetMapping
     @PreAuthorize("hasRole('ROLE_USER')")
@@ -28,12 +28,33 @@ public class MyPageController {
         return "mypage/mypage_home";
     }
 
-    @GetMapping("/qna")
+    //적립금 내역
+//    @GetMapping("/point")
+//    @PreAuthorize("hasRole('ROLE_USER') and #loginMember.loginId == principal.username")
+//    public String myPoints(@CurrentUser Member loginMember) {
+//        savedPointService.getPoints(loginMember.getId());
+//    }
+
+//    @GetMapping("/qna")
+//    @PreAuthorize("hasRole('ROLE_USER') and #loginMember.loginId == principal.username")
+//    public  String myQnas(@RequestParam(defaultValue = "1") int page, @CurrentUser Member loginMember, Model model) {
+//        PageRequest pageRequest = PageRequest.of(page - 1, 10, Sort.by("createdAt").descending());
+//        Page<MyQnaListDto> myQnaList = qnaService.getMyQnas(loginMember.getId(), pageRequest);
+//        model.addAttribute("qnas", myQnaList);
+//        return "mypage/board/qna_list";
+//    }
+
+    @GetMapping("/info")
     @PreAuthorize("hasRole('ROLE_USER') and #loginMember.loginId == principal.username")
-    public  String myQnas(@RequestParam(defaultValue = "1") int page, @CurrentUser Member loginMember, Model model) {
-        PageRequest pageRequest = PageRequest.of(page - 1, 10, Sort.by("createdAt").descending());
-        Page<MyQnaListDto> myQnaList = qnaService.getMyQnas(loginMember.getId(), pageRequest);
-        model.addAttribute("qnas", myQnaList);
-        return "mypage/board/qna_list";
+    public String infoModifyLogin(@CurrentUser Member loginMember, Model model) {
+        model.addAttribute("loginId", loginMember.getLoginId());
+        return "mypage/member/member_info_login";
+    }
+
+    @GetMapping("/info/edit")
+    @PreAuthorize("hasRole('ROLE_USER') and #loginMember.loginId == principal.username")
+    public String infoModify(@CurrentUser Member loginMember, Model model) {
+        model.addAttribute("member", loginMember);
+        return "mypage/member/member_info_modify";
     }
 }
