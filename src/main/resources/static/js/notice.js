882f0f19901f596a7bdd8b9e435b6d41ef710f01
jvv00 <jvv0006@gmail.com>
2024-08-17 18:58:11 +0900

    modify: 관리자 페이지 공지사항 alert창 swal로 변경

--- src/main/resources/static/js/notice.js
+++ src/main/resources/static/js/notice.js
@@ -5,17 +5,35 @@ function validateBeforeSubmit() {
     let contentValueLength = contentValue.length;
 
     if (titleValue === '') {
-        alert('제목을 작성해 주세요.');
+        Swal.fire({
+            text: "제목을 작성해 주세요.",
+            showConfirmButton: true,
+            confirmButtonText: '확인',
+            customClass: mySwal,
+            buttonsStyling: false
+        });
         return false;
     }
 
     if (contentValue === '') {
-        alert('내용을 작성해 주세요.');
+        Swal.fire({
+            text: "내용을 작성해 주세요.",
+            showConfirmButton: true,
+            confirmButtonText: '확인',
+            customClass: mySwal,
+            buttonsStyling: false
+        });
         return false;
     }
 
     if (contentValueLength > 2000) {
-        alert('내용은 최대 2000자까지 작성할 수 있습니다.');
+        Swal.fire({
+            text: "내용은 최대 2000자까지 작성할 수 있습니다.",
+            showConfirmButton: true,
+            confirmButtonText: '확인',
+            customClass: mySwal,
+            buttonsStyling: false
+        });
         return false;
     }
 
@@ -29,27 +47,49 @@ function deleteNotice(noticeId) {
     let csrfHeader = $("meta[name='_csrf_header']").attr("content");
     let csrfToken = $("meta[name='_csrf']").attr("content");
 
-    if (confirm("삭제하시겠습니까?")) {
-        $.ajax({
-            type: 'DELETE',
-            url: '/ajax/admin/notice/delete',
-            async: false,
-            data: {noticeId: noticeId},
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader(csrfHeader, csrfToken)
-            },
-            success: function (result) {
-                if (result.code === 200) {
-                    window.location.href = '/admin/notice';
-                } else {
-                    alert(reult.message);
+    Swal.fire({
+        text: '삭제하시겠습니까?',
+        showCancelButton: true,
+        cancelButtonText: '아니요',
+        confirmButtonText: '예',
+        closeOnConfirm: false,
+        closeOnCancel: true,
+        customClass: mySwalConfirm,
+        reverseButtons: true,
+        buttonsStyling: false,
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                type: 'DELETE',
+                url: '/ajax/admin/notice/delete',
+                async: false,
+                data: {noticeId: noticeId},
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader(csrfHeader, csrfToken)
+                },
+                success: function (result) {
+                    if (result.code === 200) {
+                        window.location.href = '/admin/notice';
+                    } else {
+                        Swal.fire({
+                            text: result.message,
+                            showConfirmButton: true,
+                            confirmButtonText: '확인',
+                            customClass: mySwal,
+                            buttonsStyling: false
+                        });
+                    }
+                },
+                error: function () {
+                    Swal.fire({
+                        html: "삭제 중 문제가 발생했습니다.<br>다시 시도해 주세요.",
+                        showConfirmButton: true,
+                        confirmButtonText: '확인',
+                        customClass: mySwal,
+                        buttonsStyling: false
+                    });
                 }
-            },
-            error: function () {
-                alert('삭제 중 오류가 발생했습니다. 다시 시도해 주세요.');
-            }
-        })
-    } else {
-        return false;
-    }
-};
+            });
+        }
+    });
+}
