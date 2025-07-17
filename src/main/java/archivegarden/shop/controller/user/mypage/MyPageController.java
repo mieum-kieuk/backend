0c6c259aa65c4e45ebeceffc1e63d15c6ed79550
olive-3 <olive3three@gmail.com>
2025-07-17 22:13:27 +0900

    refactor: MyPageController에서 상품문의 관련 로직 MyInquiryController로 분리 & pagination_simple 맨 처음과 마지막의 경우 disabled추가해 클릭안되게 수정

--- src/main/java/archivegarden/shop/controller/user/mypage/MyPageController.java
+++ src/main/java/archivegarden/shop/controller/user/mypage/MyPageController.java
@@ -1,22 +1,19 @@
 package archivegarden.shop.controller.user.mypage;
 
-import archivegarden.shop.dto.user.community.inquiry.MyInquiryListDto;
 import archivegarden.shop.dto.user.member.EditMemberInfoForm;
 import archivegarden.shop.entity.Member;
-import archivegarden.shop.service.user.community.InquiryService;
 import archivegarden.shop.service.user.member.MemberService;
-import archivegarden.shop.util.PageRequestUtil;
 import archivegarden.shop.web.annotation.CurrentUser;
-import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/mypage")
@@ -24,8 +21,6 @@ import org.springframework.web.bind.annotation.*;
 public class MyPageController {
 
     private final MemberService memberService;
-    private final InquiryService inquiryService;
-
     /**
      * 개인 정보 수정을 위한 본인확인(로그인) 페이지를 반환하는 메서드
      */
@@ -59,22 +54,5 @@ public class MyPageController {
 
         memberService.editMemberInfo(loginMember.getId(), form);
         return "redirect:/mypage/info";
-    }
-
-    @Operation(
-            summary = "내 상품 문의 목록 조회",
-            description = "사용자가 작성한 상품 문의 목록을 페이징하여 조회합니다."
-    )
-    @GetMapping("/inquiries")
-    @PreAuthorize("hasRole('ROLE_USER')")
-    public  String myInquiries(
-            @RequestParam(value = "page", defaultValue = "1") int page,
-            @CurrentUser Member loginMember,
-            Model model
-    ) {
-        PageRequest pageRequest = PageRequestUtil.of(page, 1);
-        Page<MyInquiryListDto> inquiryListDtos = inquiryService.getMyInquires(loginMember.getId(), pageRequest);
-        model.addAttribute("inquiries", inquiryListDtos);
-        return "user/mypage/inquiry/inquiry_list";
     }
 }
--- src/main/java/archivegarden/shop/controller/user/mypage/MyWishController.java
+++ src/main/java/archivegarden/shop/controller/user/mypage/MyWishController.java
@@ -4,6 +4,7 @@ import archivegarden.shop.dto.user.wish.MyWishDto;
 import archivegarden.shop.entity.Member;
 import archivegarden.shop.service.product.WishService;
 import archivegarden.shop.web.annotation.CurrentUser;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,21 +13,27 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Tag(name = "위시리스트", description = "사용자 페이지에서 위시리스트 관련 API")
 @Controller
+@RequestMapping("/mypage/wish")
 @RequiredArgsConstructor
-public class WishController {
+public class MyWishController {
 
     private final WishService wishService;
 
     /**
      * 위시 상품 목록을 조회하는 요청을 처리하는 메서드
      */
-    @GetMapping("/mypage/wish")
-    @PreAuthorize("hasRole('ROLE_USER') and #loginMember.loginId == principal.username")
-    public String wishlist(@RequestParam(name = "page", defaultValue = "1") int page, @CurrentUser Member loginMember, Model model) {
+    @GetMapping
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public String wishlist(
+            @RequestParam(name = "page", defaultValue = "1") int page,
+            @CurrentUser Member loginMember,
+            Model model
+    ) {
         PageRequest pageRequest = PageRequest.of(page - 1, 10, Sort.Direction.DESC, "createdAt");
         Page<MyWishDto> wishlist = wishService.getWishList(loginMember.getId(), pageRequest);
         model.addAttribute("products", wishlist);
