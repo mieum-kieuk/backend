e75eb0f848fe1a49635c81c9d3111329a0dbf581
jvv00 <jvv0006@gmail.com>
2025-07-26 03:30:49 +0900

    feat: 모바일 반응형 메뉴 구현

--- src/main/resources/static/js/common/comm.js
+++ src/main/resources/static/js/common/comm.js
@@ -28,13 +28,13 @@ const mySwalConfirm = {
 //         now = next
 //     }, 10000)
 // })
-$(document).ready(function() {
+$(document).ready(function () {
 
     const wrapper = $('.wrapper');
     const width = $(window).innerWidth() - 490;
     wrapper.css('min-width', width + 'px');
 
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
 
         if ($(this).scrollTop() > 80) {
             $('#header').addClass('scrolled');
@@ -42,9 +42,6 @@ $(document).ready(function() {
             $('#header').removeClass('scrolled');
         }
     });
-
-    $('#header').css('min-width', 1000 + 'px');
-    $('#footer').css('min-width', 1000 + 'px');
 
     initializeDropdownMenus();
 
@@ -57,7 +54,7 @@ $(document).ready(function() {
             $(this).val(trimmedValue);
         }
     });
-    $('.phone_number input[type="number"]').on('input', function() {
+    $('.phone_number input[type="number"]').on('input', function () {
         let maxLength = $(this).attr('maxlength');
         let value = $(this).val();
 
@@ -65,13 +62,13 @@ $(document).ready(function() {
             $(this).val(value.slice(0, maxLength));
         }
     });
-    $('#phonenumber3').on('input', function() {
+    $('#phonenumber3').on('input', function () {
         let value = $(this).val();
         if (value.length > 4) {
             $(this).val(value.slice(0, 4)); // 최대 4자리까지만 입력 가능
         }
     });
-    $('.search_btn').click(function(event) {
+    $('.search_btn').click(function (event) {
         let keyword = $('.search_form_wrap input[type="text"]').val().trim();
 
         if (keyword === '') {
@@ -85,7 +82,7 @@ $(document).ready(function() {
             });
         }
     });
-    $('.search_btn2').click(function(event) {
+    $('.search_btn2').click(function (event) {
         let keyword = $('#search_complete input[type="text"]').val().trim();
 
         if (keyword === '') {
@@ -99,35 +96,55 @@ $(document).ready(function() {
             });
         }
     });
-    $('.search button').click(function() {
+    $('.search button').click(function () {
         $('.search_popup').fadeIn();
     });
 
-    $('.search_bg').click(function() {
+    $('.search_bg').click(function () {
         $('.search_popup').fadeOut();
     });
 
-    $('#search_complete').click(function(e) {
+    $('#search_complete').click(function (e) {
         e.stopPropagation();
     });
 
     initializeSideMenu();
 
-});
+    // 모바일일 때만 작동
+    $(".hamburger").on("click", function () {
+        $(this).toggleClass("active");
+        $(".mobile_menu_bg").toggleClass("active"); // 메뉴 열고 닫기
+    });
+    function isMobile() {
+        return window.innerWidth <= 768;
+    }
 
+    $(".depth1_menu > a, .depth1_menu > span").on("click", function (e) {
+        if (isMobile()) {
+            const depth1 = $(this).parent();
+            const hasDepth2 = depth1.find(".depth2").length > 0;
+
+            if (hasDepth2) {
+                e.preventDefault();
+                depth1.toggleClass("active");
+                depth1.siblings().removeClass("active");
+            }
+        }
+    });
+});
 function initializeDropdownMenus() {
     // Document 클릭 이벤트
-    $(document).on('click', function(event) {
+    $(document).on('click', function (event) {
         closeDropdownMenus(event);
     });
 
     // 메뉴 토글 클릭 이벤트
-    $(document).on('click', '.menu_toggle', function() {
+    $(document).on('click', '.menu_toggle', function () {
         toggleDropdownMenu($(this));
     });
 
     // 드롭다운 메뉴 내 버튼 클릭 이벤트
-    $(document).on('click', '.dropdown_menu button', function() {
+    $(document).on('click', '.dropdown_menu button', function () {
         closeDropdownMenus();
     });
 }
@@ -158,7 +175,7 @@ function initializeSideMenu() {
     let url = window.location.pathname;
 
     // 현재 페이지 URL과 일치하는 depth2 항목을 찾아 active 클래스를 추가하고, 해당 depth2만 열기
-    $('.side_menu .depth2 li[data-path]').each(function() {
+    $('.side_menu .depth2 li[data-path]').each(function () {
         let dataPath = $(this).data('path');
 
         if (url.endsWith('/' + dataPath)) {
@@ -168,7 +185,7 @@ function initializeSideMenu() {
         }
     });
 
-    $('.side_menu .depth1 > li').each(function() {
+    $('.side_menu .depth1 > li').each(function () {
         let depth1 = $(this);
         let depth2 = depth1.find('.depth2');
 
@@ -177,7 +194,7 @@ function initializeSideMenu() {
             return;
         }
 
-        depth1.children('a').click(function(event) {
+        depth1.children('a').click(function (event) {
             event.preventDefault(); // 기본 링크 동작 방지
 
             // 클릭한 li의 부모 depth2를 토글
